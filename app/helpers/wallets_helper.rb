1514e03b5f9d957920f1aca7a60f6e7ae5aced74
gb23 <gregbenj@hotmail.com>
2017-10-07 15:21:59 -0700

    create wallet_for method

--- app/helpers/wallets_helper.rb
+++ app/helpers/wallets_helper.rb
@@ -3,4 +3,8 @@ module WalletsHelper
     def empty? (wallet)
         wallet.total_coins == 0 ? true : false
     end
+
+    def wallet_for(current_user, name)
+        Wallet.where(["user_id = ? and name = ?", current_user.id, name]).first
+    end
 end
