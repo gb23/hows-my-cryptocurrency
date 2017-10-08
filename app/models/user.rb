5adde4ef95ef1969be5c78873a6af1744cf6034e
gb23 <gregbenj@hotmail.com>
2017-10-08 15:17:52 -0700

    get user data from OAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,20 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable 
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook] 
   
   #when user is created, user gets wallets of all coin types
   after_create :give_wallets
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.name = auth.info.name
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+end
   
   def update_numbers
     final_adjusted = 0
@@ -34,7 +44,7 @@ class User < ApplicationRecord
 
   def give_wallets
     Coin.all.each do |coin|
-          self.wallets.create(name: coin.name, coin_id: coin.id )
+          self.wallets.create(name: coin.name, coin_id: coin.id ) if coin.name == "Bitcoin" || coin.name == "Ethereum" || coin.name == "Litecoin"
     end
   end
 
