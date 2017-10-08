eb00b530ac3ec6f28800ca160b1288284b268f6b
gb23 <gregbenj@hotmail.com>
2017-10-08 13:57:57 -0700

    add return after redirect_to so rest of action method is not executed (if @wallet is nil, don't want other methods being called on it)

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -9,8 +9,9 @@ class WalletsController < ApplicationController
     def show
         if !authenticate(@wallet)
             redirect_to user_wallets_path(current_user), notice: "Only your own wallets are viewable."
+            return
         end
-        @wallet.get_and_save_current_price 
+        @wallet.get_and_save_current_price
         # @wallet.coin.last_value = PriceScraper.send("get_#{@wallet.coin.name.downcase}_price")
         # @wallet.coin.save
         @wallet.update_wallet_with_coin_value
@@ -39,12 +40,16 @@ class WalletsController < ApplicationController
     private
 
     def find_wallet
-        @wallet = Wallet.find(params[:id])
+        @wallet = Wallet.find_by(id: params[:id])
     end
 
     def authenticate(wallet)
-        current_user.wallets.find do |w|
-            w.id == wallet.id 
+        if wallet.nil?
+            false
+        else
+            current_user.wallets.find do |w|
+                w.id == wallet.id 
+            end
         end
     end
 end
