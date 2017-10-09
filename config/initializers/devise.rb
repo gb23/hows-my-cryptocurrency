7d20f75859596da4967d6866a00ff3d187e69ddd
gb23 <gregbenj@hotmail.com>
2017-10-08 20:03:56 -0700

    add omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -250,12 +250,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-<<<<<<< HEAD
   config.omniauth :facebook, 'ENV[FACEBOOK_KEY]', 'ENV[FACEBOOK_SECRET]'
-=======
-  config.omniauth :facebook, '', ''
->>>>>>> eff187bf1e3ecb60d50c6cf1beab8d79f849d142
-
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
