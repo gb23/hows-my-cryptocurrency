f7b7e8edb7778398de804d08837b42f1f045f65b
gb23 <gregbenj@hotmail.com>
2017-10-11 11:18:53 -0700

    add big_3 method

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def big_3?(coin)
+    coin.name == "Bitcoin" || coin.name == "Ethereum" || coin.name == "Litecoin"
+  end
 end
