0051fcf7ff395360d66992517d1e6762b7bc99a7
gb23 <gregbenj@hotmail.com>
2017-10-11 11:18:13 -0700

    refactor code

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -12,30 +12,22 @@ class WalletsController < ApplicationController
             return
         end
         @wallet.get_and_save_current_price
-        # @wallet.coin.last_value = PriceScraper.send("get_#{@wallet.coin.name.downcase}_price")
-        # @wallet.coin.save
         @wallet.update_wallet_with_coin_value
     end
 
     def create
-
         if params[:commit] == "Calculate With Theoretical Price"
-            #using params user_id, you now have handle for all the wallets
             u = User.find(params[:user_id])
             w = u.wallets.find_by(name: params[:coin][:name])
             @wallet = Wallet.new(name: w.name, user_id: u.id, total_coins: w.total_coins, money_in: w.money_in, net_unadjusted: w.net_unadjusted, net_adjusted: w.net_adjusted )
             
             @wallet.coin = Coin.new(name: params[:coin][:name], last_value: params[:coin][:last_value])
-        # transactions = Transaction.where(["user_id = ? and coin_id = ?", u.id, Coin.find_by(name: params[:coin][:name]).id]).to_a
+
             @wallet.calculate_unadjusted
             @wallet.calculate_adjusted
             render :show
-
         end
-
     end
-
-   
 
     private
 
