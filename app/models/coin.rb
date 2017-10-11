b5fd86b9757aaa6fe20f10238470dbb233d6c2c6
gb23 <gregbenj@hotmail.com>
2017-10-11 14:34:01 -0700

    add class method most_transactions

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -5,5 +5,13 @@ class Coin < ApplicationRecord
 
     validates :name, presence: { message: "Coin name must be valid" }
     validates :last_value, numericality: {greater_than: 0, message: "Trading price must be greater than 0"}
+
+    def self.most_transactions
+        coin_hash = {}
+        self.all.each do |coin|
+            coin_hash[coin.name] =  coin.transactions.size
+        end
+        coin_hash.sort_by{|key, value| value}.reverse
+    end
     
 end
