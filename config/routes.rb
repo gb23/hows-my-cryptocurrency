4450ae6eef5767bae8f5df2ef4534a5976222189
gb23 <gregbenj@hotmail.com>
2017-10-11 14:35:34 -0700

    add coins#show route

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#to_user_show"
   resources :coins, only: [:update]
+  get '/coins/most_transactions' => 'coins#show'
 end
