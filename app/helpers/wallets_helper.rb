d6129549d83bd755c1d75a7993783adfd11e706b
gb23 <gregbenj@hotmail.com>
2017-10-10 20:19:36 -0700

    create <li> tag helper for wallet

--- app/helpers/wallets_helper.rb
+++ app/helpers/wallets_helper.rb
@@ -7,4 +7,18 @@ module WalletsHelper
     def wallet_for(current_user, name)
         Wallet.where(["user_id = ? and name = ?", current_user.id, name]).first
     end
+
+    def wallet_list_item_open(current_user, wallet)
+        if current_user.wallets.last == wallet
+            "<li class='list ph3 pv2'>".html_safe
+        else
+            "<li class='list ph3 pv2 bb b--light-silver'>".html_safe 
+        end
+    end
+
+    def big_3_coins_in?(wallet)
+        wallet.coin.name == "Ethereum" || wallet.coin.name == "Bitcoin" || wallet.coin.name == "Litecoin"
+    end
 end
+
+
