22a4b3e5562011bff335a6f3fe09c6cde06ab4da
gb23 <gregbenj@hotmail.com>
2017-10-10 20:19:01 -0700

    add option for color formatting of money display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,9 +11,16 @@ module ApplicationHelper
         htm.html_safe 
     end
 
-    def format_dollar_amount_of (amount)
+    def format_dollar_amount_of (amount, color) 
+        if amount.instance_of?(String)
+            amount = amount.to_f
+        end
         display_amount = number_with_delimiter(('%.2f' % amount), :delimiter => ',')
-        apply_color(amount, display_amount)
+        if color.empty?
+            "<span> $#{display_amount} </span>".html_safe 
+        else
+            apply_color(amount, display_amount)
+        end
     end
 
     def apply_color(amount, display_amount)
@@ -24,5 +31,19 @@ module ApplicationHelper
         else
             "<span> $#{display_amount} </span>".html_safe 
         end
+    end
+
+    def list_item_open(params, transaction)
+        if params[:action] != "show" || params[:controller] != "transactions"
+            if current_user.transactions.last == transaction
+                "<li class='list ph3 pv2'>".html_safe
+            else
+                "<li class='list ph3 pv2 bb b--light-silver'>".html_safe 
+            end
+        end
+    end
+
+    def list_item_close(params)
+        "</li>".html_safe if params[:action] != "show" || params[:controller] != "transactions"
     end
 end
