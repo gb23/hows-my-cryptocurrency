d35375b1f3add9dc4b9f4e299de772406eb0ce05
gb23 <gregbenj@hotmail.com>
2017-10-11 11:19:11 -0700

    clean up validation message for last_name

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -4,8 +4,6 @@ class Coin < ApplicationRecord
     has_many :users, :through => :transactions
 
     validates :name, presence: { message: "Coin name must be valid" }
-    validates :last_value, numericality: {greater_than: 0, message: "Current trading price must be greater than 0"}
+    validates :last_value, numericality: {greater_than: 0, message: "Trading price must be greater than 0"}
     
-
-
 end
