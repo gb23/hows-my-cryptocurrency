ca196fe0b3a1644a1b12cf702cf62d5816631341
gb23 <gregbenj@hotmail.com>
2017-12-03 16:53:07 -0700

    switch comment type to be string

--- db/migrate/20171201184816_create_notes.rb
+++ db/migrate/20171201184816_create_notes.rb
@@ -2,7 +2,7 @@ class CreateNotes < ActiveRecord::Migration[5.1]
   def change
     create_table :notes do |t|
       t.integer :transaction_id
-      t.text :comment
+      t.string :comment
 
       t.timestamps
     end
