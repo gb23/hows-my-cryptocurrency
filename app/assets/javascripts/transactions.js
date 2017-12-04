94eda02961ad2f6e7ff4b4a73693327d0b38c80c
gb23 <gregbenj@hotmail.com>
2017-12-04 12:36:23 -0700

    add toggle when click on ellipses icon; infomation show view is presented and ellipses icon changes to down caret.  Clicking on down caret restores ellipses view.

--- app/assets/javascripts/transactions.js
+++ app/assets/javascripts/transactions.js
@@ -11,8 +11,10 @@ $(function() {
    $("[id=transaction-show-more-icon]").on('click', function(){
         const tx_id = $(this).data("txid"); 
         const user_id = $(this).data("userid");
+        const image = $(this).data("image");
+        const $thisIcon = $(this)
         $.getJSON(`/users/${user_id}/transactions/${tx_id}`,function(APIresponse){
-            showExtraTxDataTemplate(APIresponse);
+            changeView(image, $thisIcon, APIresponse, tx_id); 
         }); 
    });
 });
@@ -24,10 +26,36 @@ function showTxsWithTemplate(txs) {
     const txList = template(txs);
     $("#transactions_in_wallet").html(txList);
 }
-function showExtraTxDataTemplate(APIresponse) {
+function showExtraTxDataTemplate(txAdditionalInfo, tx_id) {
     //insert-show-more-<%=transaction.id%>
+    const src = $("#transaction-show-template").html();
+    
+    const template = Handlebars.compile(src);
+    const txExtra = template(txAdditionalInfo);
+    $(`#insert-show-more-${tx_id}`).html(txExtra);
 }
 function changeListLink(coinName){
     $("#list_txs_in_wallet").replaceWith(`<p id="list_txs_in_wallet" class="mt0 black-80 b ph3 pv2 ba f6 dib">${coinName} Transaction History</p>`)
 }
 
+function changeView(displayedImage, $icon, APIresponse, tx_id) {
+    if (displayedImage === "ellipses"){
+        showExtraTxDataTemplate(APIresponse, tx_id);
+        changeToDownArrowIcon($icon);
+       
+    } else if (displayedImage === "downArrow") {
+        $(`#insert-show-more-${tx_id}`).empty();
+        changetoEllipsesIcon($icon);
+    }
+}
+
+function changeToDownArrowIcon($icon) {
+    $icon.data('image', "downArrow");
+    $icon.removeClass("fa-ellipsis-h")
+    $icon.addClass("fa-caret-down")
+}
+function changetoEllipsesIcon($icon) {
+     $icon.data('image', "ellipses");
+     $icon.removeClass("fa-caret-down");
+     $icon.addClass("fa-ellipsis-h");
+}
